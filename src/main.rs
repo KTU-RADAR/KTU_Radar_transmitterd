d22773a71f5c5bfa248412f77c7b0933a068d3c4
ravine1802 <ravine1802@proton.me>
2022-12-03 16:54:08 +0300

    create api for teacher announcements

--- src/main.rs
+++ src/main.rs
@@ -37,6 +37,7 @@ async fn main() {
     let app = Router::new()
         // `GET /` goes to `root`
         .route("/", get(ktu_pc_duyuru))
+        .route("/hocaduyuru", get(hoca_duyuru))
         .route("/duyuruekle", post(create_duyuru));
     // `POST /users` goes to `create_user`
     // run it with hyper on localhost:3000
@@ -44,6 +45,29 @@ async fn main() {
         .serve(app.into_make_service())
         .await
         .unwrap();
+}
+
+async fn hoca_duyuru() -> impl IntoResponse {
+    let conn = sqlite_connection();
+    let mut stmt = conn.prepare("SELECT * FROM duyurular").unwrap();
+    let person_iter = stmt
+        .query_map([], |row| {
+            Ok(CreateDuyuru {
+                hoca: row.get(0)?,
+                konu: row.get(1)?,
+                metin: row.get(2)?,
+                tarih: row.get(3)?,
+            })
+        })
+        .unwrap();
+
+    let mut jsoned_hoca_duyuru: Vec<_> = vec![];
+    for person in person_iter {
+        let cloned_person = person.unwrap().clone();
+        println!("Found person {:?}", cloned_person.clone());
+        jsoned_hoca_duyuru.push(cloned_person)
+    }
+    Json(jsoned_hoca_duyuru)
 }
 
 #[derive(Deserialize, Debug, Serialize, Clone)]
